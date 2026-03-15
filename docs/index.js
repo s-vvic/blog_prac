f348132d54be8fe9b8f65956aac459b065b9b183
손우승 <thsdntmd6547@gmail.com>
2026-03-15 22:14:48 +0900

    tmp test code

--- docs/index.js
+++ docs/index.js
@@ -12,8 +12,13 @@ window.addEventListener("DOMContentLoaded", () => {
   const cancelBtn = document.getElementById("bulk-delete-cancel-btn");
   const confirmBtn = document.getElementById("bulk-delete-confirm-btn");
 
+  console.log("버튼 찾음:", modeBtn);
+
   if (modeBtn) {
-    modeBtn.addEventListener("click", () => toggleDeleteMode(true));
+    modeBtn.addEventListener("click", () => {
+      console.log("삭제 모드 버튼 클릭됨!"); // 클릭했을 때 이 로그가 찍히는지 확인
+      toggleDeleteMode(true);
+    });
   }
 
   if (cancelBtn) {
